af71bc0efadc5f597b408fd251a18679a414865b
Tejaswi123803 <karritejaswi186@gmail.com>
2025-07-22 21:27:38 +0530

    initial commit

---
+++ Sum1.py
@@ -0,0 +1,9 @@
+#Extract unique digits from tuple list
+n=int(input())
+digits=set()
+for _ in range(n):
+    a,b=input().split()
+    digits.update(a)
+    digits.update(b)
+unique_digits=sorted(set(filter(str.isdigit,digits)))
+print("The extracted digits:", ' '.join(unique_digits))
